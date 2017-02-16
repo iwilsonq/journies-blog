7a32bfa904a5b18daf04148e858c20797ae0b7e8
iwilsonq <iwilsonq@gmail.com>
2017-02-15 20:55:36 -0800

    latest build

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -32,18 +32,24 @@ export default class Article extends Component {
                 const attrs = paragraph.split(' ')
                 const gist = attrs[0].slice(2);
                 const file = attrs[1].slice(2);
-
                 return <Gist
                   key={i}
                   gist={gist}
                   file={file}
-                />
+                />;
+              } else if (paragraph.slice(0,3) === '###') {
+                return <h3>{paragraph.slice(3)}</h3>;
+              } else if (paragraph.slice(0,2) === '##') {
+                return <h2>paragraph.slice(2)</h2>;
+              } else if (paragraph.slice(0,1) === '#') {
+                return <h1>{paragraph.slice(1)}</h1>;
+              } else {
+                return <p
+                  key={i}
+                  dangerouslySetInnerHTML={{__html: markdownParser(paragraph)}}
+                />;
               }
 
-              return <p
-                key={i}
-                dangerouslySetInnerHTML={{__html: markdownParser(paragraph)}}
-              />;
             })}
           </div>
           <Divider />
