bde6f3f721f56e4f29b2f1bab6b424c5fcca90dd
iwilsonq <iwilsonq@gmail.com>
2017-03-05 13:07:41 -0800

    hero card looks much nicer, no more need to load anything

--- src/components/Home/Hero.js
+++ src/components/Home/Hero.js
@@ -5,37 +5,32 @@ import { urlify } from '../../utils/helpers';
 export default class Hero extends Component {
   render() {
     const { articles } = this.props;
-
-    if (articles.length) {
-      return (
-        <div className="hero container">
-          <div className="welcome row">
-            <div className="col-md-6">
-              <h2 style={{ borderBottom: '2px solid #fff'}}>Ian Wilson</h2>
-              <h2>Developer, Blogger, and Stoic</h2>
-            </div>
-            <div className="col-md-6">
-              <div className="hero-card">
+    return (
+      <div className="hero container">
+        <div className="welcome row">
+          <div className="col-md-6">
+            <h2 style={{ borderBottom: '2px solid #fff'}}>Ian Wilson</h2>
+            <h2>Developer, Blogger, and Stoic</h2>
+          </div>
+          <div className="col-md-6">
+            <div className="hero-card">
+              <div className="center-align">
                 <img className="rounded" src="img/rico.jpg" alt="" />
-                <div className="icon-set">
-                  <i className="fa fa-code"></i>
-                  <i className="fa fa-coffee"></i>
-                  <i className="fa fa-cubes"></i>
-                </div>
-                <div className="icon-set space-around">
-                  <i className="fa fa-crosshairs"></i>
-                  <i className="fa fa-gamepad"></i>
-                  <i className="fa fa-edit"></i>
-                </div>
+              </div>
+              <div className="icon-set">
+                <i className="fa fa-code"></i>
+                <i className="fa fa-coffee"></i>
+                <i className="fa fa-cubes"></i>
+              </div>
+              <div className="icon-set space-around">
+                <i className="fa fa-crosshairs"></i>
+                <i className="fa fa-gamepad"></i>
+                <i className="fa fa-edit"></i>
               </div>
             </div>
           </div>
         </div>
-      );
-    }
-
-    return <div className="hero">
-      <h1>...</h1>
-    </div>;
+      </div>
+    );
   }
 };
